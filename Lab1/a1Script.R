5985c557b23ad8340b00b8506645036ed73b5cc3
Axel Andersson <axean602@student.liu.se>
2022-12-09 11:01:56 +0100

    Updates on data handling of the csv files

--- Lab1/a1Script.R
+++ Lab1/a1Script.R
@@ -2,11 +2,13 @@
 # -------------------------------------- Part 1 ------------------------------------------
 # ----------------------------------------------------------------------------------------
 # Reading the data
-data <- read.csv("data/optidigits.csv")
+data <- read.csv("data/optidigits.csv", header = F)
+data2 <- read.csv("data/optidigits.csv")
 
 # Dividing in to training, validation and test
 # n is the number of rows in optdigits.csv. Data[1] means we just count one column, so we only
 # care about the nr of rows. 
+library(dplyr)
 n=dim(data)[1]
 set.seed(12345) 
 # id is 1911 randomly distributed integers between 1 and 3822.
@@ -24,7 +26,7 @@ test=data[id3,]
 # -------------------------------------- Part 2 ------------------------------------------
 # ----------------------------------------------------------------------------------------
 # install.packages("kknn")
-# library(kknn)
+ library(kknn)
 
 # tilde sign means that left is a function of right, where right = "." means all columns except
 # the left side assigned columns. same as doing X0.26~X0.19 + X0.13....
@@ -36,27 +38,27 @@ test=data[id3,]
 # indexes. So: if for row one we get that 1, 1637 and 1845 are the most similar (k=3), 
 # and their CL values are 0, we conclude that the fitted value is (0+0+0)/3 = 0
 # Factoring 
-train$X0.26 <- as.factor(train$X0.26)
-test$X0.26 <- as.factor(test$X0.26)
+train$V65 <- as.factor(train$V65)
+test$V65 <- as.factor(test$V65)
 
-kknn_train = kknn(X0.26~., train=train, test=train, kernel="rectangular", k = 30)
-kknn_test = kknn(X0.26~., train=train, test=test, kernel="rectangular", k = 30)
+kknn_train = kknn(V65~., train=train, test=train, kernel="rectangular", k = 30)
+kknn_test = kknn(V65~., train=train, test=test, kernel="rectangular", k = 30)
 
 # Predictions
 train_predict = predict(kknn_train)
 test_predict = predict(kknn_test)
 
 # Confusion matrices
-conf_train <- table(train_predict, train$X0.26)
-conf_test <- table(test_predict, test$X0.26)
+conf_train <- table(train_predict, train$V65)
+conf_test <- table(test_predict, test$V65)
 
 missclass <- function(X,Y) {
   n=length(X)
   return(1-sum(diag(table(X,Y)))/n)
 }
 
-missclass_train <- missclass(train_predict, train$X0.26)
-missclass_test <- missclass(test_predict, test$X0.26)
+missclass_train <- missclass(train_predict, train$V65)
+missclass_test <- missclass(test_predict, test$V65)
 
 #Comments on prediction quality - TRAIN: 1:s quite often confused with eights, and 9:s often confused 
 # With fives. Missclass: 0.04239. TEST: Worse performance, missclass 0.04916. Quite often we think 
@@ -73,10 +75,10 @@ missclass_test <- missclass(test_predict, test$X0.26)
 # Seven in row 2. 
 
 # all eights just gets all indexes from train data where hit was an eight.
-all_eights <- train[which(train$X0.26 == "8"),]
+all_eights <- train[which(train$V65 == "8"),]
 # eight_probs first filters out the 1911x10 probability numerics from kknn_train, then sorts by 
 # the rows (by index) that had eights. 
-eight_probs <- kknn_train[["prob"]][which(train$X0.26 == "8"),9]
+eight_probs <- kknn_train[["prob"]][which(train$V65 == "8"),9]
 worst_probs <- sort(eight_probs, decreasing=F, index.return=T)$ix[1:3]
 best_probs <- sort(eight_probs, decreasing=T, index.return=T)$ix[1:2]
 
@@ -100,20 +102,20 @@ multi.missclass.valid <- c()
 
 for(x in 1:30) {
   #Train
-  multi.train.kknn <- kknn(X0.26 ~., train = train, test = train, k = x, kernel = "rectangular")
+  multi.train.kknn <- kknn(V65 ~., train = train, test = train, k = x, kernel = "rectangular")
   pred.train <- multi.train.kknn$fitted.values
-  multi.missclass.train <- c(multi.missclass.train, missclass(pred.train, train$X0.26))
+  multi.missclass.train <- c(multi.missclass.train, missclass(pred.train, train$V65))
   #Valid
-  multi.valid.kknn <- kknn(X0.26 ~., train = train, test = valid, k = x, kernel = "rectangular")
+  multi.valid.kknn <- kknn(V65 ~., train = train, test = valid, k = x, kernel = "rectangular")
   pred.valid <- multi.valid.kknn$fitted.values
-  multi.missclass.valid <- c(multi.missclass.valid, missclass(pred.valid, valid$X0.26))
+  multi.missclass.valid <- c(multi.missclass.valid, missclass(pred.valid, valid$V65))
 }
 
 best_k <- which.min(multi.missclass.valid)
 
-test1.kknn <- kknn(X0.26 ~., train=train, test=test, k = best_k, kernel="rectangular")
+test1.kknn <- kknn(V65 ~., train=train, test=test, k = best_k, kernel="rectangular")
 pred.test1 <- test1.kknn$fitted.values
-missclass_bestk <- missclass(pred.test1, test$X0.26)
+missclass_bestk <- missclass(pred.test1, test$V65)
 
 
 #Create first line
@@ -133,7 +135,7 @@ lines(best_k, missclass_bestk*100, pch=1, type="b")
 multi_cross_entropy <- c()
 loopsize <- nrow(valid)
 for (i in 1:30) {
-  multi_cross_kknn <- kknn(X0.26 ~., train = train, test = valid, k = i, kernel = "rectangular")
+  multi_cross_kknn <- kknn(V65 ~., train = train, test = valid, k = i, kernel = "rectangular")
   cross_kknn_prob <- multi_cross_kknn[["prob"]]
   ce_value = 0
   for (current in 0:9) {
@@ -149,12 +151,12 @@ for (i in 1:30) {
   # char_cross <- as.character(pred.cross)
   # numeric_predicts <- as.numeric(char_cross)
   # 
-  # multi_cross_entropy <- c(multi_cross_entropy, cross_entropy(valid$X0.26, numeric_predicts))
+  # multi_cross_entropy <- c(multi_cross_entropy, cross_entropy(valid$V65, numeric_predicts))
 
 print(multi_cross_entropy)
 plot(multi_cross_entropy, col="red")
 
-
+which.min(multi_cross_entropy)
 
 
 
--- Lab2/A3Script.R
+++ Lab2/A3Script.R
@@ -1,12 +1,17 @@
 # Assignment 3
 library(dplyr)
 library(caret)
-data <- read.csv("Data/communities.csv")
+#---------------------------------------------------------------------------------
+#------------------------------------Task 1---------------------------------------
+#---------------------------------------------------------------------------------
+data <- read.csv("Data/communities.csv", stringsAsFactors = T)
 data_wo_VCPP <- data%>%select(-ViolentCrimesPerPop)
 
 scaler <- preProcess(data_wo_VCPP)
-scaled_data <- cbind(predict(scaler, newdata = data_wo_VCPP),data$ViolentCrimesPerPop)
-cov = cov()
+scaled_data <- as.data.frame(cbind(predict(scaler, newdata = data_wo_VCPP),data$ViolentCrimesPerPop))
+covariance = cov(scaled_data)
+eigen_vals <- eigen(covariance, only.values = T)
 #---------------------------------------------------------------------------------
 #------------------------------------Task 2---------------------------------------
 #---------------------------------------------------------------------------------
+
